8a7199b4e3a65cff3caee7a904bf20ba1b2754ef
Craig Mayhew <craig@mayhew.io>
2017-09-04 00:12:00 +0100

    Improved cloud formation name

--- rai/cloudformation.tf
+++ rai/cloudformation.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_cloudformation_stack" "rai_API_Gateway_and_Lambdas" {
   capabilities = ["CAPABILITY_IAM"]
-  name = "apigatewaylambdas"
+  name = "Rai-apigateway-lambdas"
   on_failure = "DELETE"
   template_body = "${ file("apigateway_lambdas.yml") }"
 }
