0bb9b6b0b9d9abc378995cb818847e6193335605
Craig Mayhew <craig@mayhew.io>
2017-09-12 14:02:08 +0100

    Moving to S3 for index.html - Attempted

--- rai/cloudformation.tf
+++ rai/cloudformation.tf
@@ -2,9 +2,17 @@ provider "aws" {
   region     = "eu-west-2"
 }
 
+resource "aws_s3_bucket_object" "index" {
+  bucket = "rai.mayhew.io"
+  key    = "index.html"
+  source = "s3/index.html"
+  etag   = "${md5(file("s3/index.html"))}"
+}
+
 resource "aws_cloudformation_stack" "rai_Lambdas" {
   capabilities = ["CAPABILITY_IAM"]
   name = "Rai-lambdas"
   on_failure = "DELETE"
   template_body = "${ file("lambdas.yml") }"
 }
+
